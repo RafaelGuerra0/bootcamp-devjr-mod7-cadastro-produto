e1dedbbbb33de5e40f91e240462296b5369b59cb
RafaelGuerra0 <rafael.albiero.guerra@outlook.com>
2024-11-05 17:23:36 -0300

    products exception

--- product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
@@ -3,28 +3,24 @@ package com.abutua.product_backend.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.abutua.product_backend.models.Product;
 
-
+import jakarta.annotation.PostConstruct;
 
 @RestController
 public class ProductController {
-    @GetMapping("product")
-    public Product getProduct(){
 
-        Product p = new Product();
-        p.setId(1);
-        p.setName("Product 01");
-        p.setPrice(100.50);
-        return p;
+    private List<Product> products = new ArrayList<>();
 
-
-    }
-    @GetMapping("products")  
-    public List<Product> getProducts(){
+    @PostConstruct // apos a construcao do objeto product controller, o metodo init() sera chamado
+    public void init() {
         Product p1 = new Product();
         p1.setId(1);
         p1.setName("Product 01");
@@ -40,17 +36,32 @@ public class ProductController {
         p3.setName("Product 03");
         p3.setPrice(300.50);
 
-        List <Product> listProd = new ArrayList<>();
-        listProd.add(p1);
-        listProd.add(p1);
-        listProd.add(p3);
-        return listProd;
-
+        products.add(p1);
+        products.add(p2);
+        products.add(p3);
     }
 
+    @GetMapping("products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {    // @PathVariable pega a variavel que veio no caminho la no endpoint, no caso, variavel id
+        //                                                                     responseentity devolve status code, ex 404
 
+        // if (id<=products.size() && id>0){
+        //     return ResponseEntity.ok(products.get(id-1));
+        // }else{
+        //     //return ResponseEntity.notFound().build();
+        //     throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+        // }
+        Product prod = products.stream()  //programacao funcional
+                                .filter(p -> p.getId() == id)
+                                .findFirst()
+                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+                                // ao lancar uma excessao, as proximas linhas nao serao executadas
+        return ResponseEntity.ok(prod);
+    }
 
+    @GetMapping("products")
+    public List<Product> getProducts() {
+        return products;
+    }
 
 }
-    
-
