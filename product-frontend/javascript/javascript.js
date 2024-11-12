780ae80e93d23bd5313709828b879f8f2721996d
RafaelGuerra0 <rafael.albiero.guerra@outlook.com>
2024-11-12 15:55:23 -0300

    conserto do front end

--- product-frontend/javascript/javascript.js
+++ product-frontend/javascript/javascript.js
@@ -1,5 +1,6 @@
 initialIndex =3;
 currentIndex=initialIndex;
+
 function FuncAddProd(){
     varName = String(document.getElementById("name").value);
     console.log(varName);
@@ -26,4 +27,81 @@ function FuncAddProd(){
     
 
 
+}
+
+
+var products = [
+    {
+        id: 1,
+        name: "Computador",
+        description: "Descricao PC",
+        price: 4900.00,
+        category: 1,
+        onSale: true,
+        newProduct: true
+    },
+    {
+        id: 2,
+        name: "Computador 2",
+        description: "Descricao PC",
+        price: 4900.00,
+        category: 1,
+        onSale: true,
+        newProduct: true
+    },
+    {
+        id: 3,
+        name: "Computador 3",
+        description: "Descricao PC",
+        price: 4900.00,
+        category: 1,
+        onSale: true,
+        newProduct: true
+    }
+];
+// usar o do java
+var categories = [
+    {id:1, name: "Producao Propria"},
+    {id:2, name: "Nacional"},
+    {id:3, name: "Importado"}
+];
+
+
+LoadProducts();
+function  LoadProducts(){
+    for(let prod of products){
+        AddNewRow(prod);
+    }
+
+}
+function AddNewRow(prod){
+    var table = document.getElementById("productsTable");
+    
+    var newRow = table.insertRow();
+
+    var idNode = document.createTextNode(prod.id);
+    newRow.insertCell().appendChild(idNode);
+
+    var nameNode = document.createTextNode(prod.name);
+    newRow.insertCell().appendChild(nameNode);
+
+    var descriptionNode = document.createTextNode(prod.description);
+    newRow.insertCell().appendChild(descriptionNode);
+
+    var priceNode = document.createTextNode(prod.price);
+    newRow.insertCell().appendChild(priceNode);
+
+    var categoryNode = document.createTextNode(categories[prod.category-1].name);
+    newRow.insertCell().appendChild(categoryNode);
+
+    var options = '';
+    if (prod.onSale){
+        options = '<span class = "badge bg-success">P</span>';
+
+    }
+    if (prod.newProduct){
+        options = '<span class = "badge bg-primary">L</span>';
+
+    }
+    newRow.insertCell().innerHTML+=options;
 }
